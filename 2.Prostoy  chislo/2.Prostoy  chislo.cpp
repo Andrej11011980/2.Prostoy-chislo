e2bcb95e95e44e32f72f707b51294e104f8de03b
Андрей Боярский <an0982978490@outlook.com>
2022-04-07 11:10:50 +0300

    Добавьте файлы проекта.

---
+++ 2.Prostoy  chislo/2.Prostoy  chislo.cpp
@@ -0,0 +1,23 @@
+﻿//2.	Написать функцию, которая проверяет, является ли переданное ей число простым.Число называется простым, если оно делится без остатка только на себя и на единицу.
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+void ShowProstoe(int number) {
+
+
+	bool prostoe = true;
+	for (int i = 2; i < number; i++) {
+		if (number % i == 0) prostoe = false;
+	}
+	if (!prostoe) cout << "Число не является простым!!!\n";
+	else cout << "Число является простым!!!\n";
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	ShowProstoe(5);
+}
